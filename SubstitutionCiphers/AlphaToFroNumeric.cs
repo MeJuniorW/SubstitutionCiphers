c784e91115d84eaf786ad3d869117d2c7b4183e2
Junior Muriithi <murithiwagura@gmail.com>
2021-05-03 01:13:27 +0300

    decryption for A1Z26 cipher

--- SubstitutionCiphers/AlphaToFroNumeric.cs
+++ SubstitutionCiphers/AlphaToFroNumeric.cs
@@ -9,36 +9,64 @@ namespace SubstitutionCiphers
     class AlphaToFroNumeric
     {
         
-        public static char[] GetAlphabet()
+        public static Dictionary<char, int> GetEncryptionChartIntValues()
         {
             char[] alphabet = Enumerable.Range('A', 26).Select(x => (char)x).ToArray();
-            return alphabet;
+            Dictionary<char, int> A1Z26refChart = new Dictionary<char, int>();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                A1Z26refChart.Add(alphabet[i], i + 1);
+            }
+            return A1Z26refChart;
+        }
+        public static Dictionary<int, char> GetEncryptionChartCharValues()
+        {
+            char[] alphabet = Enumerable.Range('A', 26).Select(x => (char)x).ToArray();
+            Dictionary<int, char> A1Z26refChart = new Dictionary<int, char>();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                A1Z26refChart.Add(i + 1, alphabet[i]);
+            }
+            return A1Z26refChart;
         }
         public static string EncryptAlphaToFroNumeric(string unencryptedText)
         {
-            char[] charsToIgnore = { ',', ' ', '/', ':', ';', '-', '.' };
-            char[] alphabet = GetAlphabet();
+            char[] charsToIgnore = { ',', ' ', '/', ':', ';', '-', '.', '"', '[', ']', '{', '}', '=', '_', '(', ')', '*', '&', '^', '%', '$', '#', '@', '!', '`', '~' };
+            Dictionary<char, int> encryptionChart = GetEncryptionChartIntValues();
             string[] encrypted = new string[unencryptedText.Length];
             for (int i = 0; i < unencryptedText.Length; i++)
             {
-                if (!charsToIgnore.Contains(unencryptedText[i]))
+                if (encryptionChart.TryGetValue(unencryptedText[i], out int value))
                 {
-                    string numericValue = (Array.IndexOf(alphabet, unencryptedText[i]) + 1).ToString();
-                    encrypted[i] = numericValue;
+                    encrypted[i] = value.ToString();
                 }
                 else
                 {
                     encrypted[i] = "0";
                 }
             }
-            string encryptedText = string.Join('-', encrypted);
-            return encryptedText;
+            return string.Join('-', encrypted);
         }
 
         public static string DecryptAlphaToFroNumeric(string encryptedText)
         {
-
-            return "";
+            char[] seperators = { ',', ' ', '/', ':', ';', '-', '.', '"', '[', ']', '{', '}', '=', '_', '(', ')', '*', '&', '^', '%', '$', '#', '@', '!', '`', '~' };
+            string[] encryptionValues = encryptedText.Split(seperators);
+            string decryptedText = string.Empty;
+            Dictionary<int, char> encryptionChart = GetEncryptionChartCharValues();
+            for (int i = 0; i < encryptionValues.Length; i++)
+            {
+                _ = Int32.TryParse(encryptionValues[i], out int key);
+                if (encryptionChart.TryGetValue(key, out char value))
+                {
+                    decryptedText += value;
+                }
+                else
+                {
+                    decryptedText += ' ';
+                }
+            }
+            return decryptedText;
         }
 
     }
