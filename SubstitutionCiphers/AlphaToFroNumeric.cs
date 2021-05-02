ff0cb120ea450c1e4922fb6cd00b077c1430673c
Junior Muriithi <murithiwagura@gmail.com>
2021-05-02 22:04:26 +0300

    Add project files.

---
+++ SubstitutionCiphers/AlphaToFroNumeric.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstitutionCiphers
+{
+    class AlphaToFroNumeric
+    {
+        
+        public static char[] GetAlphabet()
+        {
+            char[] alphabet = Enumerable.Range('A', 26).Select(x => (char)x).ToArray();
+            return alphabet;
+        }
+        public static string EncryptAlphaToFroNumeric(string text)
+        {
+            char[] charsToIgnore = { ',', ' ', '/', ':', ';', '-', '.' };
+            char[] alphabet = GetAlphabet();
+            string[] unencrypted = text.Split(charsToIgnore);
+            text = unencrypted.ToString();
+            string[] encrypted = new string[text.Length];
+            string encryptedText = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                string numericValue = (Array.IndexOf(alphabet, text[i]) + 1).ToString();
+                encrypted[i] = numericValue;
+            }
+            encryptedText = string.Join('-', encrypted);
+            return encryptedText;
+        }
+
+        public static string DecryptAlphaToFroNumeric(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SubstitutionCiphers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SubstitutionCiphers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Choice();
+        }
+        public static void Choice()
+        {
+            // Console.WriteLine("Do you wish to encrypt or decrypt ( e / d )\nChoice : ");
+            // string choice = Console.ReadLine().ToLower();
+            // Console.WriteLine("Enter Text: ");
+            string text = Console.ReadLine().ToUpper();
+            string encryptedText = AlphaToFroNumeric.EncryptAlphaToFroNumeric(text);
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
